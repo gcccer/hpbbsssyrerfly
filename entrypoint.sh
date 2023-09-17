7e50100cbe1b14725ec5811d1f9d368253c5c651
gcccer <129157292+gcccer@users.noreply.github.com>
2023-09-17 08:42:17 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'c664751b-b662-48f9-b2aa-2f0d4d337a7f'}
+UUID=${UUID:-'6e26839e-4246-43d9-abdf-daca5254f220'}
 
 # 其他Paas保活
 PAAS1_URL=
@@ -17,8 +17,8 @@ KOYEB_ACCOUNT=
 KOYEB_PASSWORD=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"4ce79e5abd95bfd2a63f6675102157a6","TunnelSecret":"tKhsZKPNjvKxJn9IkMHDuVPGk8A+8BWNmMbtYCtlRVc=","TunnelID":"723647b5-ac24-4094-a247-c39ffafe5f47"}'
+ARGO_DOMAIN=slimpooreww.ossroad.link
 
 generate_config() {
   cat > config.json << EOF
